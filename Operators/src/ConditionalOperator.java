c4a20a36afa88a937496607fd24fa0f42fb93385
Priyanshi Rana <ranaprachi2007@gmail.com>
2026-07-11 12:58:33 +0530

    commit collection

---
+++ Operators/src/ConditionalOperator.java
@@ -0,0 +1,15 @@
+
+public class ConditionalOperator {
+      public static void main(String[] args) {
+    	int a=10,b=5;
+    	
+  		int c=a++<b--?a++*++b/a:--b%a++-++b/b--;
+  		
+  		System.out.println(a);
+  		
+  		System.out.println(b);
+  		
+  		System.out.println(c);
+	}
+      
+}
---
+++ Operators/src/IncrementDecrement5.java
@@ -0,0 +1,8 @@
+
+public class IncrementDecrement5 {
+      public static void main(String[] args) {
+    	int x=10;
+  		x=x++;
+  		System.out.println(x);
+      }
+}
